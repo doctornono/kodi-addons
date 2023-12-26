1ad2d60347ff1a180c410467679c8fb115cf9314
doctornono <44597041+doctornono@users.noreply.github.com>
2023-12-26 03:50:48 +0100

    up

--- noosfere.py
+++ noosfere.py
@@ -3,6 +3,7 @@ import urllib
 from urllib import request
 
 import re
+import scraperNoosfere
 
 url = 'https://www.noosfere.org/livres/collection.asp?numcollection=1975551973'
 req = urllib.request.urlopen(url)
@@ -10,54 +11,28 @@ response = req.read()
 req.close()
 soup = BeautifulSoup(response, 'html.parser')
 #print(soup)
-tableau = soup.find_all('table', class_="noocadre_pad5")[0]
-lignes = tableau.find_all('tr')
+tableau = soup.find('table', class_="noocadre_pad5")
+table = tableau.find_next('table')
+lignes = table.find_all('tr')
 for ligne in lignes:
     #print(ligne, '++++++++++++++++++')
     numero =  str(ligne.find('td').get_text())
     lien = ligne.find_all('a')
     titre = lien[0].get_text()
     fiche = lien[0]['href']
-
-    #print(titre, numero)
-    expression_reguliere = r'numitem=(\d+)'
-
-    # Recherche de la correspondance dans le fiche
-    correspondance = re.search(expression_reguliere, fiche)
-
-    # Vérification de la correspondance et extraction de la valeur de numitem
-    if correspondance:
-        valeur_numitem = correspondance.group(1)
-        #print("La valeur de numitem est :", valeur_numitem)
-    else:
-       # print("Aucune correspondance trouvée.")
-        valeur_numitem = None
-
-    url = 'https://www.noosfere.org/livres/' + fiche.replace('./', '')
+    # Extraction du ISBN
+    match = re.search(r'./EditionsLivre\.asp\?numitem=(\d+)', str(ligne))
+    numitem = match.group(1).strip() if match else None
+     
+    print(numero,  titre,  numitem)
+    """
+    url = 'https://www.noosfere.org/livres/EditionsLivre.asp?numitem=' + numitem
     req = urllib.request.urlopen(url)
     response = req.read()
     req.close()
     soupbook = BeautifulSoup(response, 'html.parser')
     title = soupbook.title.get_text()
-    #print(title)
-    if  title.find('Fiche livre'):
-        #print(title)
-        dd = soupbook.find_all('td', class_="vousetesici")
-        #ddd = dd.find_all('a')
-        #print(str(dd))
-        expression_reguliere = r'choix\.asp\?numlivre=(\d+)'
-
-        # Recherche de la correspondance dans le fiche
-        correspondance = re.search(expression_reguliere, str(dd))
-
-        # Vérification de la correspondance et extraction de la valeur de numitem
-        if correspondance:
-            valeur_numlivre = correspondance.group(1)
-           # print("La valeur de numlivre est :", valeur_numlivre)
-        else:
-            #print("Aucune correspondance trouvée.")
-            valeur_numlivre =None
-        couv = soupbook.find_all('div', id="quatrieme")
-        print(titre, numero, valeur_numitem, valeur_numlivre)
-        print('-----------------')
-        print(couv)
+    print(str(title))
+    """
+myscrap = scraperNoosfere
+print(str(myscrap.scraperNoosfere.scrapLivre(456)))
---
+++ scraperNoosfere.py
@@ -0,0 +1,184 @@
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+import json
+
+import re
+
+class scraperNoosfere:
+    def scrapLivre(numlivre):
+        url = "https://www.noosfere.org/livres/niourf.asp?numlivre=2146579743"
+
+        # Envoi de la requête HTTP
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        response = urlopen(req)
+
+        # Vérification de la réussite de la requête
+        if response.getcode() == 200:
+            # Analyse du contenu de la page avec BeautifulSoup et le parseur html.parser
+            soup = BeautifulSoup(response.read(), 'html.parser')
+
+
+            # Extraction des informations
+            titre = soup.find('span', {'class': 'TitreNiourf'}).text.strip()
+            auteur = soup.find('span', {'class': 'AuteurNiourf'}).text.strip()
+
+            fiche = str(soup.find('span', {'class': 'ficheNiourf'}))
+            ficheauteur = str(soup.find('span', {'class': 'AuteurNiourf'}))
+
+            # Extraction du titre original
+            match = re.search(r'Titre original\s*:\s*<i>([^,]+)', fiche, re.IGNORECASE)
+            titreoriginal = match.group(1).strip() if match else None
+
+            # Extraction de l'année originale
+            match = re.search(r'Titre original\s*:\s*<i>[^,]+,\s*(.*?)</i>', fiche, re.IGNORECASE)
+            anneeoriginale = match.group(1).strip() if match else None    
+
+            # Extraction de l'illustrateur
+            match = re.search(r'Illustration de\s+<a[^>]+>(.*?)</a>', fiche, re.DOTALL)
+            illustration = match.group(1).strip() if match else None    
+
+            # Extraction de l'illustrateur
+            match = re.search(r'Traduction de\s+<a[^>]+>(.*?)</a>', fiche, re.DOTALL)
+            traduction = match.group(1).strip() if match else None    
+
+            # Extraction du cycle
+            match = re.search(r'Cycle : <a[^>]+>(.*?)</a>', fiche, re.DOTALL)
+            serie = match.group(1).strip() if match else None
+
+            # Extraction du tome
+            match = re.search(r'vol\.\s+(\d+)', fiche)
+            tome = match.group(1).strip() if match else None
+
+            # Extraction de l'éditeur
+            match = re.search(r'<a href="editeur.asp[^>]+>(.*?)</a>', fiche, re.DOTALL)
+            editeur = match.group(1).strip() if match else None
+
+            # Extraction de la collection
+            match = re.search(r'coll\.\s+<a[^>]+>(.*?)</a>', fiche, re.DOTALL)
+            collection = match.group(1).strip() if match else None
+
+            # Extraction du numéro de collection
+            match = re.search(r'n°\s+(\d+)', fiche)
+            id = match.group(1).strip() if match else None
+
+            sousfiche = str(soup.find('span', {'class': 'sousFicheNiourf'}))
+            
+            # Extraction du depot legal
+            match = re.search(r'Dépôt légal : <a[^>]+>(.*?)</a>', sousfiche)
+            depotlegal = match.group(1).strip() if match else None
+            
+            # Extraction du nombre de pages
+            match = re.search(r'(\d+) pages', sousfiche, re.DOTALL)
+            pages = match.group(1).strip() if match else None
+
+            # Extraction du ISBN
+            match = re.search(r'ISBN : (\S+)', sousfiche)
+            isbn = match.group(1).strip() if match else None
+            
+            # Extraction du genre
+            match = re.search(r'Genre : (.*?)<br/>', sousfiche, re.DOTALL)
+            genre = match.group(1).strip() if match else None
+
+            # Extraction de l'edition
+            match = re.search(r'Réédition', sousfiche, re.DOTALL)
+            if match:
+                edition = 'Réédition'
+            match = re.search(r'Première édition', sousfiche, re.DOTALL)
+            if match:
+                edition = 'Première édition'    
+
+            # Extraction de la categorie comme Roman, ...
+        
+            match = re.search(r'Roman', sousfiche, re.DOTALL)
+            if match:
+                categorie = 'Roman'
+            match = re.search(r'Recueil de nouvelles', sousfiche, re.DOTALL)
+            if match:
+                categorie = 'Recueil de nouvelles'        
+            match = re.search(r'Recueil de romans', sousfiche, re.DOTALL)
+            if match:
+                categorie = 'Recueil de romans'  
+            match = re.search(r'Anthologie', sousfiche, re.DOTALL)
+            if match:
+                categorie = 'Anthologie'  
+            match = re.search(r'Biographie', sousfiche, re.DOTALL)
+            if match:
+                categorie = 'Biographie'
+            match = re.search(r'Revue', sousfiche, re.DOTALL)
+            if match:
+                categorie = 'Revue'        
+            match = re.search(r'Novella', sousfiche, re.DOTALL)
+            if match:
+                categorie = 'Novella'  
+
+
+            quatrieme = str(soup.find('div', {'id': 'quatrieme'}))
+
+            urlcouv = soup.find('img', {'name': 'couverture'})['src']
+
+
+
+
+            # ******** ID NOOSFERE ********
+            match = re.search(r'<a[^>]+href="/livres/auteur.asp\?NumAuteur=(-?\d+)".*?>(.*?)</a>' , ficheauteur, re.DOTALL)
+            id_auteur = match.group(1).strip() if match else None
+
+            match = re.search(r'https://www.noosfere.org/livres/niourf.asp\?numlivre=(\d+)', url, re.DOTALL)
+            id_livre = match.group(1).strip() if match else None
+
+            match = re.search(r'<a[^>]+href="editeur.asp\?numediteur=(\d+)"[^>]*>(.*?)</a>', fiche, re.DOTALL)
+            id_editeur = match.group(1).strip() if match else None
+
+            match = re.search(r'<a[^>]+href="collection.asp\?NumCollection=(-?\d+)&amp;numediteur=\d+"[^>]*>(.*?)</a>', fiche, re.DOTALL)
+            id_collection = match.group(1).strip() if match else None
+
+            match = re.search(r'Cycle\s*:\s*<a[^>]+serie.asp\?numserie=(\d+)[^>]*>(.*?)</a>', fiche, re.DOTALL)
+            id_serie = match.group(1).strip() if match else None
+
+            match = re.search(r'Illustration de\s*<a[^>]+auteur.asp\?NumAuteur=(\d+)[^>]*>(.*?)</a>', fiche, re.DOTALL)
+            id_illustrateur = match.group(1).strip() if match else None
+
+            match = re.search(r'Traduction de\s*<a[^>]+auteur.asp\?NumAuteur=(\d+)[^>]*>(.*?)</a>', fiche, re.DOTALL)
+            id_traducteur = match.group(1).strip() if match else None
+
+
+
+            # Création d'un dictionnaire pour stocker les informations
+            livre_info = {
+                'titre': titre,
+                'auteur': auteur,
+                'titreoriginal' : titreoriginal,
+                'anneeoriginale' : anneeoriginale,
+                'illustration' : illustration,
+                'traduction' : traduction,
+                'serie' : serie,
+                'tome' : tome,
+                'categorie' : categorie,
+                'collection' : collection,
+                'collection_id' : id,
+                'depotlegal' : depotlegal,
+                'pages' : pages,
+                'genre' : genre,
+                'quatrieme' : quatrieme,
+                'isbn' : isbn,
+                'editeur' : editeur,
+                'edition' : edition,
+                'urlcouv' : urlcouv,
+                # ID NOOSFERE
+                'id_livre' : id_livre,
+                'id_auteur' : id_auteur,
+                'id_illustrateur' : id_illustrateur,
+                'id_traducteur' : id_traducteur,
+                'id_serie' : id_serie,
+                'id_collection' : id_collection,
+                'id_editeur' : id_editeur
+            }
+
+            # Convertir le dictionnaire en format JSON
+            livre_json = json.dumps(livre_info, ensure_ascii=False, indent=2)
+
+            # Afficher le JSON
+            return livre_json
+
+        else:
+            print("La requête a échoué. Statut de la requête :", response.getcode())
